3bc95773d17766a5b0005fae0f75e0eca17c76cd
Alumno Mañana <Angel Jesus>
2021-10-15 12:09:04 +0200

    Nombre,Localidad,Año de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,8 @@
 class Prueba {
   public static void main(String [] args){
       System.out.println("Angel Jesus Santin Betancourt");
+	System.out.println("Ponferrada,1999");
+	System.out.println("Compilando Prueba.java");
+	
   }
 }
