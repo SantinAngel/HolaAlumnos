2a8651491c18ab87329e36cded42b4460cdd47f8
Alumno Mañana <Angel Jesus>
2021-10-19 09:02:02 +0200

    cantante

---
+++ cantante.java
@@ -0,0 +1,8 @@
+class cantante {
+  public static void main(String [] args){
+	System.out.println("Ponferrada,1999");
+	System.out.println("Compilando Prueba.java");
+	System.out.println("Leon,Windows 10");
+  	System.out.println("MGK");
+	}
+}
